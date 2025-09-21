7c5e3c9fe15fd9b7e550465ce0cbc29316b0d96f
Bartosz Art <bartosz.art@gmail.com>
2025-09-21 20:20:35 +0200

    FEAT: create user GQL

--- apps/api/src/main.module.ts
+++ apps/api/src/main.module.ts
@@ -2,12 +2,12 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
 import { PostModule } from './post/post.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
-import { CommentModule } from './comment/comment.module';
 import { TagModule } from './tag/tag.module';
-import { LikeModule } from './like/like.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 8000);
 }
 
